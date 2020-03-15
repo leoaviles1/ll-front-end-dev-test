8d2a49663e7cb1cd71df9eb6af77f408f2c58e87
leoaviles1 <leoaviles93@gmail.com>
2020-03-15 00:30:14 -0700

    Added mobile toggle button and JS that helps animate the mobile button. Added event listeners that help with bringing the mobile nav down and closing it.

--- app.js
+++ app.js
@@ -15,3 +15,23 @@ var images = {
     path: "img/beast.jpg"
   }
 };
+
+//Event Listener for mobile menu toggle button
+document.querySelector(".mobile-toggle").addEventListener("click", function() {
+  mobileNavToggle();
+});
+
+//Event Listener to exit mobile menu when link is clicked
+document.querySelectorAll(".mobile-nav-link").forEach(item => {
+  item.addEventListener("click", event => {
+    mobileNavToggle();
+  });
+});
+
+function mobileNavToggle() {
+  //this will add active class to mobile-toggle to animate the button
+  document.querySelector(".mobile-toggle").classList.toggle("active");
+
+  //this will add show class to the mobile-nav-wrapper
+  document.querySelector(".mobile-nav-wrapper").classList.toggle("show");
+}
